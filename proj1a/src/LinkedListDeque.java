ce21c9f82e597138401b4d7c96e4818e29a84c43
Your Name <you@berkeley.edu>
2024-01-26 18:20:13 +0200

    final

---
+++ proj1a/src/LinkedListDeque.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListDeque<T> implements Deque<T> {
+
+    public int size = 0;
+
+    /** The basic node for the Deque. */
+    private class StuffNode {
+        T value;
+        StuffNode next;
+        StuffNode prev;
+
+        StuffNode(T v, StuffNode n, StuffNode p)  {
+            value = v;
+            next = n;
+            prev = p;
+        }
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> lld = new LinkedListDeque<>();
+    }
+
+    StuffNode sentinel = new StuffNode(null,null,null);
+    public LinkedListDeque() {
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        StuffNode newNode = new StuffNode(x, sentinel.next, sentinel);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        StuffNode newNode = new StuffNode(x,sentinel,sentinel.prev);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        StuffNode p = sentinel;
+        while(p.next != sentinel) {
+            returnList.add(p.next.value);
+            p = p.next;
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if(isEmpty()) return null;
+        StuffNode newFirst = sentinel.next.next;
+        sentinel.next = newFirst;
+        newFirst.prev.next = null;
+        newFirst.prev = sentinel;
+        return newFirst.value;
+    }
+
+    @Override
+    public T removeLast() {
+        if(isEmpty()) return null;
+        StuffNode newEnd = sentinel.prev.prev;
+        newEnd.next.prev = null;
+        newEnd.next = sentinel;
+        sentinel.prev = newEnd;
+        return newEnd.value;
+    }
+
+    @Override
+    public T get(int index) {
+        if(index < 0 || index >= size) return null;
+        StuffNode p = sentinel;
+        index++;
+        while(index-- > 0) {
+            p = p.next;
+        }
+        return p.value;
+    }
+    /** Helper method to get the element using recursion */
+    private T getRecursiveHelper(int i,StuffNode p) {
+        if(i == 0) return p.value;
+
+        return getRecursiveHelper(i-1,p.next);
+    }
+    @Override
+    public T getRecursive(int index) {
+        if(index < 0 || index >= size) return null;
+        return getRecursiveHelper(index+1,sentinel);
+    }
+}
--- proj1a/tests/LinkedListDequeTest.java
+++ proj1a/tests/LinkedListDequeTest.java
@@ -11,64 +11,146 @@ import static com.google.common.truth.Truth.assertWithMessage;
 /** Performs some basic linked list tests. */
 public class LinkedListDequeTest {
 
-    // @Test
-    // @DisplayName("LinkedListDeque has no fields besides nodes and primitives")
-    // void noNonTrivialFields() {
-    //     Class<?> nodeClass = NodeChecker.getNodeClass(LinkedListDeque.class, true);
-    //     List<Field> badFields = Reflection.getFields(LinkedListDeque.class)
-    //             .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(nodeClass) || f.isSynthetic()))
-    //             .toList();
+     @Test
+     @DisplayName("LinkedListDeque has no fields besides nodes and primitives")
+     void noNonTrivialFields() {
+         Class<?> nodeClass = NodeChecker.getNodeClass(LinkedListDeque.class, true);
+         List<Field> badFields = Reflection.getFields(LinkedListDeque.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(nodeClass) || f.isSynthetic()))
+                 .toList();
 
-    //     assertWithMessage("Found fields that are not nodes or primitives").that(badFields).isEmpty();
-    // }
+         assertWithMessage("Found fields that are not nodes or primitives").that(badFields).isEmpty();
+     }
 
-    // @Test
-    // /** In this test, we have three different assert statements that verify that addFirst works correctly. */
-    // public void addFirstTestBasic() {
-    //     Deque<String> lld1 = new LinkedListDeque<>();
+     @Test
+     /** In this test, we have three different assert statements that verify that addFirst works correctly. */
+     public void addFirstTestBasic() {
+         Deque<String> lld1 = new LinkedListDeque<>();
 
-    //     lld1.addFirst("back"); // after this call we expect: ["back"]
-    //     assertThat(lld1.toList()).containsExactly("back").inOrder();
+         lld1.addFirst("back"); // after this call we expect: ["back"]
+         assertThat(lld1.toList()).containsExactly("back").inOrder();
 
-    //     lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
-    //     assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
+         lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
+         assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
 
-    //     lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
-    //     assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+         lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
+         assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
 
-    //     /* Note: The first two assertThat statements aren't really necessary. For example, it's hard
-    //        to imagine a bug in your code that would lead to ["front"] and ["front", "middle"] failing,
-    //        but not ["front", "middle", "back"].
-    //      */
-    // }
+         /* Note: The first two assertThat statements aren't really necessary. For example, it's hard
+            to imagine a bug in your code that would lead to ["front"] and ["front", "middle"] failing,
+            but not ["front", "middle", "back"].
+          */
+     }
 
-    // @Test
-    // /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
-    //  *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
-    // public void addLastTestBasic() {
-    //     Deque<String> lld1 = new LinkedListDeque<>();
+     @Test
+     /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
+      *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
+     public void addLastTestBasic() {
+         Deque<String> lld1 = new LinkedListDeque<>();
 
-    //     lld1.addLast("front"); // after this call we expect: ["front"]
-    //     lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
-    //     lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
-    //     assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
-    // }
+         lld1.addLast("front"); // after this call we expect: ["front"]
+         lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
+         lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
+         assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+     }
 
-    // @Test
-    // /** This test performs interspersed addFirst and addLast calls. */
-    // public void addFirstAndAddLastTest() {
-    //     Deque<Integer> lld1 = new LinkedListDeque<>();
+     @Test
+     /** This test performs interspersed addFirst and addLast calls. */
+     public void addFirstAndAddLastTest() {
+         Deque<Integer> lld1 = new LinkedListDeque<>();
 
-    //     /* I've decided to add in comments the state after each call for the convenience of the
-    //        person reading this test. Some programmers might consider this excessively verbose. */
-    //     lld1.addLast(0);   // [0]
-    //     lld1.addLast(1);   // [0, 1]
-    //     lld1.addFirst(-1); // [-1, 0, 1]
-    //     lld1.addLast(2);   // [-1, 0, 1, 2]
-    //     lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+         lld1.addLast(0);   // [0]
+         lld1.addLast(1);   // [0, 1]
+         lld1.addFirst(-1); // [-1, 0, 1]
+         lld1.addLast(2);   // [-1, 0, 1, 2]
+         lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
 
-    //     assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
-    // }
+         assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+     }
 
-    // Below, you'll write your own tests for LinkedListDeque.
+//     Below, you'll write your own tests for LinkedListDeque.
+    @Test
+    public void SizeTest() {
+         Deque<Integer> lld = new LinkedListDeque<>();
+         assertThat(lld.size()).isEqualTo(0);
+         lld.addLast(45);
+         lld.addLast(45);
+         lld.addLast(45);
+         lld.addFirst(56);
+         lld.addFirst(56);
+         lld.addFirst(56);
+         assertThat(lld.size()).isEqualTo(6);
+    }
+    @Test
+    public void IsEmptyTest() {
+         Deque<Integer> lld = new LinkedListDeque<>();
+         assertThat(lld.isEmpty()).isTrue();
+         lld.addFirst(4);
+         assertThat(lld.isEmpty()).isFalse();
+         lld.addFirst(6);
+         assertThat(lld.isEmpty()).isFalse();
+    }
+    @Test
+    public void getTest() {
+        Deque<Integer> lld = new LinkedListDeque<>();
+        assertThat(lld.get(0)).isNull();
+        lld.addLast(1);
+        lld.addLast(2);
+        lld.addLast(3);
+        lld.addLast(4);
+        assertThat(lld.get(0)).isEqualTo(1);
+        assertThat(lld.get(1)).isEqualTo(2);
+        assertThat(lld.get(2)).isEqualTo(3);
+        assertThat(lld.get(3)).isEqualTo(4);
+        lld.addFirst(6);
+        assertThat(lld.get(0)).isEqualTo(6);
+    }
+    @Test
+    public void getRecursiveTest() {
+        Deque<Integer> lld = new LinkedListDeque<>();
+        assertThat(lld.getRecursive(0)).isNull();
+        lld.addLast(1);
+        lld.addLast(2);
+        lld.addLast(3);
+        lld.addLast(4);
+        assertThat(lld.getRecursive(0)).isEqualTo(1);
+        assertThat(lld.getRecursive(1)).isEqualTo(2);
+        assertThat(lld.getRecursive(2)).isEqualTo(3);
+        assertThat(lld.getRecursive(3)).isEqualTo(4);
+        lld.addFirst(6);
+        assertThat(lld.getRecursive(0)).isEqualTo(6);
+    }
+
+    @Test
+    public void removeFirstTestBasic() {
+        Deque<Integer> lld = new LinkedListDeque<>();
+        lld.addFirst(5);
+        lld.addFirst(4);
+        lld.addFirst(3);
+        lld.removeFirst();
+        assertThat(lld.get(0)).isEqualTo(4);
+        lld.removeFirst();
+        assertThat(lld.get(0)).isEqualTo(5);
+        lld.removeFirst();
+        assertThat(lld.get(0)).isEqualTo(null);
+    }
+    @Test
+    public void removeLastTestBasic() {
+        Deque<String> lld1 = new LinkedListDeque<>();
+        lld1.addFirst("back");
+        assertThat(lld1.toList()).containsExactly("back").inOrder();
+        lld1.addFirst("red");
+        lld1.addFirst("blue");
+        lld1.addFirst("white");
+        assertThat(lld1.toList()).containsExactly("white","blue","red","back").inOrder();
+        lld1.removeLast();
+        assertThat(lld1.toList()).containsExactly("white","blue","red").inOrder();
+        lld1.removeLast();
+        assertThat(lld1.toList()).containsExactly("white","blue").inOrder();
+        lld1.addFirst("hunter");
+        assertThat(lld1.removeLast()).isEqualTo("white");
+    }
+
 }
